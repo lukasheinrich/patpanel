0fa1a97f2a4f764cf4e07f2e608c2173152150cd
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-04-04 19:23:56 -0400

    reqs

--- server.py
+++ server.py
@@ -10,4 +10,4 @@ def home():
     return render_template('base.html', data = data)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = os.environ.get('PORT',5000))
+    app.run(host = '0.0.0.0', port = int(os.environ.get('PORT',5000)))
